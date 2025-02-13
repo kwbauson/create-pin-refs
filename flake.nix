150f9cc47fbd358983bcf1fd2b1126b4eb6025ec
Keith Bauson <kwbauson@gmail.com>
2025-02-12 20:07:28 -0500

    add a cachix workflow

--- flake.nix
+++ flake.nix
@@ -3,13 +3,20 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-    in
-    {
-      packages = lib.genAttrs lib.systems.flakeExposed (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./create-pin-refs.nix { };
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        default = self.packages.${system}.create-pin-refs;
+        create-pin-refs = pkgs.callPackage ./create-pin-refs.nix { };
+        test = pkgs.writers.writeBashBin "test" ''
+          echo this is a test package
+        '';
+        test-linux = pkgs.writers.writeBashBin "test-linux" ''
+          echo only pin this on linux
+        '';
       });
-    };
+  };
 }
