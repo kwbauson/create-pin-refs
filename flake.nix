d74a259bad0800ca65e08e2f342d04ea3d6ba3cf
Keith Bauson <kwbauson@gmail.com>
2025-02-12 20:15:53 -0500

    ahh cachix action pushes already

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
         default = self.packages.${system}.create-pin-refs;
         create-pin-refs = pkgs.callPackage ./create-pin-refs.nix { };
         test = pkgs.writers.writeBashBin "test" ''
-          echo this is a test package
+          echo "this is a test package"
         '';
         test-linux = pkgs.writers.writeBashBin "test-linux" ''
-          echo only pin this on linux
+          echo "only pin this on linux"
         '';
       });
   };
